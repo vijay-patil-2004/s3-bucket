7b597e1e66bec0114c60bd0e2da783ebaee2c9aa
vijay-patil-2004 <5662vpvijupatil2004@gmail.com>
2024-12-22 13:54:08 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "access_key" {
+  description = "AKIAXTORPVWAAOPG7N7W"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "r6fleIfBY/SBXzQ5am951HKBht5mnPfAqfnc+Wqm"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "vijays3bucket4330"
+}
